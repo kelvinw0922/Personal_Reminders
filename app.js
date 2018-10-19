f4e197915882bcc352ad18ef323bad59a668d24d
Kelvin <kelvinwong0922@gmail.com>
2018-10-19 15:12:19 -0700

    Added Edit and Remove features

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require(`express`);
 const exphbs = require(`express-handlebars`);
+const methodOverride = require("method-override");
 const bodyParser = require(`body-parser`);
 const mongoose = require(`mongoose`);
 
@@ -17,7 +18,6 @@ mongoose
   .catch(err => console.log(err));
 
 // Load Reminders Model
-
 require("./models/Reminders");
 const Reminder = mongoose.model("reminders");
 
@@ -29,6 +29,9 @@ app.set("view engine", "handlebars");
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse applicaiton/json
 app.use(bodyParser.json());
+
+// Method Override Middleware
+app.use(methodOverride("_method"));
 
 // Index Route
 app.get("/", (req, res) => {
@@ -98,6 +101,35 @@ app.post("/reminders", (req, res) => {
       res.redirect("/reminders");
     });
   }
+});
+
+// Edit Reminders' Form
+app.put("/reminders/:id", (req, res) => {
+  Reminder.findOne({
+    _id: req.params.id
+  }).then(reminder => {
+    // update new values
+    reminder.event_title = req.body.title;
+    reminder.details = req.body.details;
+    // save the updated reminder
+    reminder.save().then(() => {
+      res.redirect("/reminders");
+    });
+  });
+});
+
+// Delete Reminder
+app.delete("/reminders/:id", (req, res) => {
+  //res.send("DELETE");
+  Reminder.deleteOne({
+    _id: req.params.id
+  }).then(() => res.redirect("/reminders"));
+  // Also works
+  /*
+  Reminder.remove({
+    _id: req.params.id
+  }).then(() => res.redirect("/reminders"));
+  */
 });
 
 // Setting port number to 5000
