6e8fa1362dc260a57e5fb6dfc141c752c77e358a
Kelvin <kelvinwong0922@gmail.com>
2018-10-25 18:47:00 -0700

    Added Login and Register front-end layout

--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,11 @@ const mongoose = require(`mongoose`);
 
 // Authentication - User Login Route
 router.get("/login", (req, res) => {
-  res.send("login");
+  res.render("users/login");
 });
 
 router.get("/register", (req, res) => {
-  res.send("register");
+  res.render("users/register");
 });
 
 module.exports = router;
