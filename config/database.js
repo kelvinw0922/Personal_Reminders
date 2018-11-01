d98fffb50003e18578ebf20873dd747f2c48e518
Kelvin Wong <kelvinwong0922@gmail.com>
2018-10-31 17:32:04 -0700

    Delete database.js

--- config/database.js
+++
@@ -1,8 +0,0 @@
-if (process.env.NODE_ENV == "production") {
-  module.exports = {
-    mongoURI:
-      "mongodb://rozanate:tracymacno1@ds243963.mlab.com:43963/online-reminder"
-  };
-} else {
-  module.exports = { mongoURI: "mongodb://localhost/reminder-app" };
-}
